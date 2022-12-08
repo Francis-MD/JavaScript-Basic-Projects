d70ec582fecd1119c802e0ac8c9b2c41953e8489
Francis MD <francis@studio137.co.za>
2022-12-08 04:37:47 +0400

    questions completed

--- 7_Questions/app.js
+++ 7_Questions/app.js
@@ -1,2 +1,33 @@
 //using selectors inside the element
-// traversing the dom
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function (question) {
+    //console.log(question);
+    const btn = question.querySelector('.question-btn');
+    //console.log(btn);
+    //showing the text on click event
+    btn.addEventListener('click', function () {
+    //hiding non-selected text when selecting another question
+    questions.forEach(function (item) {
+        if (item !== question) {
+            item.classList.remove('show-text');
+        }
+    });
+
+        question.classList.toggle('show-text');
+    });
+});
+
+
+// TRAVERSING THE DOM
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+
+//     question.classList.toggle("show-text");
+//   });
+// });
